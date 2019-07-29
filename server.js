bc482fc508fb749519bceaef18abdc3062832409
Jonatan Acosta <jonatanacosta@MacBook-Air.local>
2019-07-29 15:12:26 -0500

    initial

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const config = require('./db');
+const UserRoute = require('./UserRoute');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/user', UserRoute);
+
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:',PORT);
+});
