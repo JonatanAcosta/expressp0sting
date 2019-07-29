fd12d26fe93876420416207e8818ad17cf8412d2
Jonatan Acosta <jonatanacosta@MacBook-Air.local>
2019-07-29 15:26:21 -0500

    modified server files

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // db.js
 
 module.exports = {
-    DB: 'mongodb://localhost:27019/expresspost'
+    DB: 'mongodb://localhost:27017/expresspost'
   }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const config = require('./db');
 const UserRoute = require('./UserRoute');
